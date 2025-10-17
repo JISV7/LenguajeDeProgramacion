ed8b22005b93230e643755b2cb6e4819c80f10e6
JASA <destructomax1@gmail.com>
2025-10-17 16:44:48 -0400

    refactor: Remove task-manager-rust to be re-added as a submodule

--- task-manager-rust/src/commands/delete.rs
+++
@@ -1,8 +0,0 @@
-use crate::error::AppResult;
-use crate::task::TaskManager;
-
-/// Processes the deletion of a task.
-pub fn execute(manager: &mut TaskManager, id: u32) -> AppResult<bool> {
-    manager.delete_task(id)?;
-    Ok(true) // Data has changed, needs to be saved.
-}
